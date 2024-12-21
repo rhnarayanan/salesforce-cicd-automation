01882ac1bca4634c977ebfeb67bc213b040306f1
rhnarayanan <rhnarayanan@gmail.com>
2024-12-21 18:57:17 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 def main():
-    print("This is the Salesforce Package.")
+    print("This is the Salesforce Package1.")
+    print("This is the Salesforce Package2.")
     print("Ready for environment-specific deployment!")
 
 if __name__ == "__main__":
